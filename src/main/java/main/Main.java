c88951e4e45ed18b3235d628aeef95b6957f9b03
ArtemMakovskyy <photomak79@gmail.com>
2022-11-06 15:29:09 +0200

    created basic classes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,8 +2,6 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "01 Commit";
-        String str02 = "02 Commit";
-        String str03 = "03 Commit";
+
     }
 }
---
+++ src/main/java/main/exception/UserService.java
@@ -0,0 +1,8 @@
+package main.exception;
+
+public class UserService {
+    public int getUserScore(String[] records, String email) {
+        //write your code here
+        return 0;
+    }
+}
