a6666a174e2939b538816d476682d5427057de98
ArtemMakovskyy <photomak79@gmail.com>
2022-11-06 15:00:29 +0200

    022

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,9 @@
 package main;
 
 public class Main {
+    public static void main(String[] args) {
+        String str = "01 Commit";
+        String str02 = "02 Commit";
+        String str03 = "03 Commit";
+    }
 }
