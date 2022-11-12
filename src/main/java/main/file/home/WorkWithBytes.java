bee8b4f9706c95ab322046a46b004b43013ce4bd
ArtemMakovskyy <photomak79@gmail.com>
2022-11-12 09:36:45 +0200

    m

--- src/main/java/main/file/home/WorkWithBytes.java
+++ src/main/java/main/file/home/WorkWithBytes.java
@@ -1,8 +1,28 @@
 package main.file.home;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-        //write your code here
-
+//        File file = new File(fileName);
+//        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, true))){
+//            file.createNewFile();
+//            for (byte b : data) {
+//                bufferedWriter.write(b );
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException("Can't write data to file " + fileName, e);
+//        }
+        File file = new File(fileName);
+        try (FileWriter fw = new FileWriter(new File(fileName))) {
+            file.createNewFile();
+            for (byte b : data) {
+                fw.write(b);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file " + fileName, e);
+        }
     }
 }
